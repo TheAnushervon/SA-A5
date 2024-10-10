292bdedacc83ace7728d7abcdf0b9246536f55e2
Anushervon Qodirzoda <a.qodirzoda@innopolis.university>
2024-10-10 18:19:05 +0300

    current state

--- message.py
+++ message.py
@@ -5,28 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 app = FastAPI()
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./messages.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-class MessageModel(Base):
-    __tablename__ = "messages"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    message = Column(Text)
-
-Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 class Message(BaseModel):
     username: str
     message: str
@@ -44,3 +22,10 @@ def add_message(message: Message, db: Session = Depends(get_db)):
 def get_messages(db: Session = Depends(get_db)):
     messages = db.query(MessageModel).all()
     return {"messages": messages}
+
+@app.get("/feed")
+def get_feed (db: Session = Depends(get_db)): 
+
+
+
+    

bfd236298470065f34f45801d7ef86858a190890
Anushervon Qodirzoda <a.qodirzoda@innopolis.university>
2024-10-10 19:05:00 +0300

    update 1.0

--- message.py
+++ message.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from models import MessageModel
+from database import get_db
 app = FastAPI()
 
 class Message(BaseModel):
@@ -25,7 +27,8 @@ def get_messages(db: Session = Depends(get_db)):
 
 @app.get("/feed")
 def get_feed (db: Session = Depends(get_db)): 
-
+    result = db.query(MessageModel).all()
+    return {"result": result}
 
 
     
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class MessageModel(Base):
-    __tablename__ = "messages"
+    __tablename__ = "db-main"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True)
     message = Column(Text)

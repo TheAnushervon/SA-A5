40da812a5ef4e41bd15261ce7101452a6d204e26
muhammaduss <muhammad20040130@gmail.com>
2024-10-11 18:48:13 +0300

    fix shell script for linux

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 
 echo "Starting FastAPI apps..."
 
-uvicorn user.auth:app --reload --host 0.0.0.0 --port 8000 &
-uvicorn message.message:app --reload --host 0.0.0.0 --port 8001 &
-uvicorn feed.feed:app --reload --host 0.0.0.0 --port 8002 &
+python3 -c "print('Running fastapi instances')" &
+uvicorn message.message:app --reload --host 0.0.0.0 --port 8000 &
+uvicorn feed.feed:app --reload --host 0.0.0.0 --port 8001 &
+uvicorn user.auth:app --reload --host 0.0.0.0 --port 8002
 
 echo "All apps started."
